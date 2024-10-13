70bce1acdbc3494c0c9477ce676a683d7864ba98
Benjamin <smartcoder199413@gmail.com>
2024-10-13 21:26:54 +0300

    upload ready

--- webpack.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    // Other configurations...
-    devServer: {
-      allowedHosts: ['localhost'], // Ensure this is a non-empty string or array of strings
-      // Other devServer options...
-    },
-  };
-  
